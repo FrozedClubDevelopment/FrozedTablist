e42f2e19152c2c938b52c7324a1ee6a6621850d3
Elb1to <elbitomc@gmail.com>
2022-08-31 02:46:02 -0500

    Fixed goofy shit

--- src/main/java/club/frozed/tablist/entry/TabEntry.java
+++ src/main/java/club/frozed/tablist/entry/TabEntry.java
@@ -1,12 +1,18 @@
 package club.frozed.tablist.entry;
 
 import club.frozed.tablist.skin.Skin;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
 
 /**
  * Created by Ryzeon
  * Project: Hatsur TabAPI
  * Date: 12/10/2020 @ 08:36
  */
+@Accessors(chain = true) @AllArgsConstructor @RequiredArgsConstructor @Getter @Setter
 public class TabEntry {
 
 	private final int column;
@@ -16,45 +22,4 @@ public class TabEntry {
 	private int ping = 1;
 	private Skin skin = Skin.DEFAULT_SKIN;
 
-	public TabEntry(int column, int row, String text, int ping, Skin skin) {
-		this.column = column;
-		this.row = row;
-		this.text = text;
-		this.ping = ping;
-		this.skin = skin;
-	}
-
-	public TabEntry(int column, int row, String text) {
-		this.column = column;
-		this.row = row;
-		this.text = text;
-	}
-
-	public int getColumn() {
-		return column;
-	}
-
-	public int getRow() {
-		return row;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public int getPing() {
-		return ping;
-	}
-
-	public void setPing(int ping) {
-		this.ping = ping;
-	}
-
-	public Skin getSkin() {
-		return skin;
-	}
-
-	public void setSkin(Skin skin) {
-		this.skin = skin;
-	}
 }
